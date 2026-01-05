da5d49b20dda954a96033283c30842412dee5404
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2026-01-05 13:08:15 +0500

    Lesson1 CLasswork.

---
+++ lesson_1/classwork.py
@@ -0,0 +1,13 @@
+a = 16
+b = 5
+c = a / b
+d = a // b
+e = a % b
+p = a ** 2
+k = divmod(a, b)
+
+print("a / b =", c)
+print("a // b =", d)
+print("a % b =", e)
+print("a ** 2 =", p)
+print("divmod(a, b) =", k)
