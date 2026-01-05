14cd67e8ee3fd5e8ab1ca6b687cc30ac19ccf89b
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2026-01-05 16:59:26 +0500

    BASE: Finish String Task 1.

---
+++ lesson_2/homework/string/task1.py
@@ -0,0 +1,6 @@
+# Create a program to ask name and year of birth from user and tell them their age.
+
+userName = input("Enter your name: ")
+userYearOfBirth = int(input("Enter year of birth: "))
+
+print(f"{userName} you are {2026 - userYearOfBirth} years old.")
