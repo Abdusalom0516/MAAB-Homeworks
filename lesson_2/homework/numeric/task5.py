227d9c0b25d7db5ef8b6aad44df92f5dca3e5fcd
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2026-01-05 16:54:24 +0500

    Finish Numeric Homework.

---
+++ lesson_2/homework/numeric/task5.py
@@ -0,0 +1,5 @@
+celcious = float(input("Enter Celcious: "))
+
+fahrenheit = celcious * 9/5 + 32
+
+print(f"Celcious {celcious} is {fahrenheit} Fahrenheit.")
---
+++ lesson_2/homework/numeric/task6.py
@@ -0,0 +1,3 @@
+number = input("Enter a number: ")
+
+print(f"Last digit of {number} is {number[-1]}.")
