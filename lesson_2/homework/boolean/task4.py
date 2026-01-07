aeb1fd86c7256063758badb942aaa5803caff03f
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2026-01-07 12:42:34 +0500

    Finish Lesson 2 Boolean HW.

---
+++ lesson_2/homework/boolean/task4.py
@@ -0,0 +1,8 @@
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+number3 = int(input("Enter third number: "))
+
+if number1 != number2 and number2 != number3 and number1 != number3:
+    print(True)
+else:
+    print(False)
---
+++ lesson_2/homework/boolean/task7.py
@@ -0,0 +1,7 @@
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
+
+if number1 + number2 > 50.8:
+    print(True)
+else:
+    print(False)
