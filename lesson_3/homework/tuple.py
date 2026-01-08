b2913dac02da1c72ad5709d99b640eb8d358e622
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2026-01-08 14:22:02 +0500

    Finish Lesson 3 Tuple HW.

--- lesson_3/homework/tuple.py
+++ lesson_3/homework/tuple.py
@@ -123,3 +123,207 @@ if myTuple:
     print("Given tuple is not empty.")
 else:
      print("Given tuple is empty.")
+
+
+
+# Task 11
+print("--------- TASK 11 -----------")
+myTuple = ("Abdusalom", "freedom", "999", "freedom", "The World", "jojo", "Anime")
+element = "freedom"
+
+indices = []
+
+for i in range(len(myTuple)):
+    if myTuple[i] == element:
+        indices.append(i)
+
+indices = tuple(indices)
+
+print(myTuple)
+print(element)
+print(indices)
+
+
+# Task 12
+print("--------- TASK 12 -----------")
+myTuple = (-2, 54, -90, 32, 77, 120)
+
+myList = list(myTuple)
+myList.sort(reverse=True)
+
+secondLargest = myList[1]
+
+
+print(myTuple)
+print(secondLargest)
+
+
+
+# Task 13
+print("--------- TASK 13 -----------")
+myTuple = (-2, 54, -90, 32, 77, 120)
+
+myList = list(myTuple)
+myList.sort()
+
+secondSmallest = myList[1]
+
+
+print(myTuple)
+print(secondSmallest)
+
+
+
+# Task 14
+print("--------- TASK 14 -----------")
+
+singleElementTuple = (999, )
+
+print(type(singleElementTuple))
+print(singleElementTuple)
+
+
+
+# Task 15
+print("--------- TASK 15 -----------")
+myList = [-2, 54, -90, 32, 77, 120]
+
+convertedToTuple = tuple(myList)
+
+print(myList)
+print(convertedToTuple)
+
+
+
+# Task 16
+print("--------- TASK 16 -----------")
+myTuple = (1, 2, 3, 4, 5)
+
+sortedTuple = list(myTuple)
+
+sortedTuple.sort()
+
+sortedTuple = tuple(sortedTuple)
+
+if myTuple == sortedTuple:
+    print("Given tuple is sorted.")
+else:
+    print("Given tuple is not sorted.")
+
+
+
+# Task 19
+print("--------- TASK 19 -----------")
+myTuple = (1, 2, 3, 4, 5)
+element = 4
+
+newTuple = ()
+newList = []
+
+for elem in myTuple:
+   if elem != element:
+        newList.append(elem)
+
+newTuple = tuple(newList)
+
+print(myTuple)
+print(element)
+print(newTuple)
+
+
+
+
+# Task 20
+print("--------- TASK 20 -----------")
+myTuple = (-1, 134, -281, -42, 5, 26, 999)
+
+converted = list(myTuple)
+
+nestedTuple= []
+
+for i in range(len(converted)):
+    if i % 2 != 0:
+        nestedTuple.append((converted[i-1], converted[i]))
+
+if len(converted) % 2 != 0:
+    nestedTuple.append((converted[len(converted)-1], ))
+
+nestedTuple = tuple(nestedTuple)
+
+print(nestedTuple)
+
+
+# Task 21
+print("--------- TASK 21 -----------")
+myTuple = (1, 2, 3, 4, 5)
+repeatNumber = 3
+
+newTuple = ()
+newList = []
+
+for elem in myTuple:
+    for i in range(repeatNumber):
+        newList.append(elem)
+
+newTuple = tuple(newList)
+
+print(myTuple)
+print(newTuple)
+
+
+# Task 22
+print("--------- TASK 22 -----------")
+rangeTuple = []
+
+for number in range(10):
+    rangeTuple.append(number+1)
+
+rangeTuple = tuple(rangeTuple)
+
+print(rangeTuple)
+
+
+
+# Task 23
+print("--------- TASK 23 -----------")
+myTuple = (1, 2, 3, 4, 5)
+
+reversedTuple = list(myTuple)
+reversedTuple.reverse()
+
+reversedTuple = tuple(reversedTuple)
+
+print(myTuple)
+print(reversedTuple)
+
+
+# Task 24
+print("--------- TASK 24 -----------")
+myTuple = (1, 2, 3, 4, 5, 4, 3, 2)
+
+reversedTuple = list(myTuple)
+reversedTuple.reverse()
+
+reversedTuple = tuple(reversedTuple)
+
+print(myTuple)
+if myTuple == reversedTuple:
+    print("Given tuple is palindrome.")
+else:
+     print("Given tuple is not palindrome.")
+
+
+# Task 25
+print("--------- TASK 25 -----------")
+myTuple = (1, 2, 3, 4, 5, 4, 3, 2)
+
+uniqueElementsTuple = []
+
+for elem in myTuple:
+    if uniqueElementsTuple.count(elem) == 0:
+        uniqueElementsTuple.append(elem)
+
+uniqueElementsTuple = tuple(uniqueElementsTuple)
+
+print(myTuple)
+print(uniqueElementsTuple)
