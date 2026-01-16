ed8b38cf3490d13503ed6336c5e31a44fc4ce031
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2026-01-16 16:10:17 +0500

    Finish Lesson 8 Task 1.

--- lesson_8/homework.py
+++ lesson_8/homework.py
@@ -13,13 +13,13 @@ class Animal:
         self.gender = gender
 
     def eat(self):
-        print(f"{type(self)} named {self.name} is eating...")
+        print(f"{self.__class__.__name__} named {self.name} is eating...")
 
     def walk(self):
-        print(f"{type(self)} named {self.name} is walking...")
+        print(f"{self.__class__.__name__} named {self.name} is walking...")
 
     def makeNoise(self):
-        print(f"{type(self)} named {self.name} is making noise...")
+        print(f"{self.__class__.__name__} named {self.name} is making noise...")
 
 
 
@@ -32,6 +32,54 @@ class Horse(Animal):
 
         self.speed = speed
         self.breed = breed
+
+    def run(self):
+        print("Horse is running...")
+
+
+
+class Cow(Animal):
+    amountMilkPerDay: float
+    isPregnant: bool
+
+    def __init__(self, *, name: str, color: str, gender: str, price: float, amountMilkPerDay: float, isPregnant: bool):
+        super().__init__(name=name, color=color, gender=gender, price=price)
+
+        self.amountMilkPerDay = amountMilkPerDay
+        self.isPregnant = isPregnant
+
+    def produceMilk(self):
+        print("Cow is producing milk...")
+
+
+class Chicken(Animal):
+    numberOfEggsPerDay: int
+
+    def __init__(self, *, name: str, color: str, gender: str, price: float, numberOfEggsPerDay: int):
+        super().__init__(name=name, color=color, gender=gender, price=price)
+
+        self.numberOfEggsPerDay = numberOfEggsPerDay
+
+
+    def layEgg(self):
+        print("Chicken is laying egg...")
+
+
+
+
+horse = Horse(name="Shadow", color="Black", gender="male", breed="Persian", price=15000, speed=58)
+cow = Cow(amountMilkPerDay=35, isPregnant=False, name="Shiny", gender="female", color="Black & White", price=4500)
+chicken = Chicken(numberOfEggsPerDay=3, name="Cocky", color="White", gender="female", price=55)
+
+horse.eat()
+cow.walk()
+chicken.makeNoise()
+
+horse.run()
+cow.produceMilk()
+chicken.layEgg()
+
+
 
 
 
