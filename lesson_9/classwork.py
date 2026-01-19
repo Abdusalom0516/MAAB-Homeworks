c71261bb12e526782aace4567a7d2c8c04591d72
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2026-01-19 14:54:56 +0500

    Practice CSV Usage.

---
+++ lesson_9/classwork.py
@@ -0,0 +1,28 @@
+# Exceptions
+
+class CustomException(Exception):
+    pass
+
+try:
+    raise CustomException("Fuck you.")
+except CustomException as ce:
+    print(ce)
+
+
+import csv
+
+with open("tasks.csv", "r") as f:
+    tasks = csv.reader(f)
+
+    for elem in tasks:
+        print(elem)
+
+
+with open("tasks.csv", "w") as f:
+    writer = csv.writer(f)
+
+    writer.writerow()
+
+    
+
+
