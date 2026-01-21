cc4f62a3bd09285e1da6520c614267727a6336d7
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2026-01-21 12:28:05 +0500

    Create Necessary Methods In Note Class.

--- lesson_9/core/note.py
+++ lesson_9/core/note.py
@@ -1,9 +1,20 @@
 from typing import Union # helps to accept multiple types of value. Or we can write date_created | str
 from datetime import datetime
-
+from __future__ import annotations
 
 class Note:
-    def __init__(self, id: int, text: str, date_created: Union[datetime, str]):
+    def __init__(self, *, id: int, text: str, date_created: Union[datetime, str]):
         self.id = id
         self.text = text
         self.date_created = date_created
+
+    def __str__(self):
+        return f"Note(id={self.id}, text={self.text}, date_created={self.date_created})"
+
+    @staticmethod
+    def fromJson(jsonData: dict[str, str]) -> Note:
+        return Note(id=jsonData["id"], text=jsonData["text"], date_created=jsonData["date_created"])
+
+    @staticmethod
+    def fromTuple(tupleData: tuple) -> Note:
+        return Note(id=tupleData[0], text=tupleData[1], date_created=tupleData[2])
