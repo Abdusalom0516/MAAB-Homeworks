1b7b82900c558428e3701bf91cc2e677b723859a
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2026-01-21 14:20:37 +0500

    Create show_menu Method.

--- lesson_9/main.py
+++ lesson_9/main.py
@@ -9,10 +9,11 @@ class ConsoleApp:
 
     def run(self):
         print("Application running...")
+        self.show_menu()
 
-    def show_notes(self) -> None:
+
+    def show_all_notes(self) -> None:
         all_notes = self.__notebook.get_notes()
-
         for note in all_notes:
             print(note)
 
@@ -32,7 +33,6 @@ class ConsoleApp:
     def update_note(self):
         note_id = int(input("Enter a note_id: "))
         new_text = input('Enter new text: ')
-
         self.__notebook.update_note(new_note= Note(id=note_id, text=new_text, date_created=datetime.now()))
 
 
@@ -41,12 +41,39 @@ class ConsoleApp:
         self.__notebook.delete_note(note_id=note_id)
 
 
+    def show_menu(self):
+        while True:
+            userOption = input('''
+1. Add Note
+2. Show All Notes
+3. Show Note by ID
+4. Update Note
+5. Delete Note
+6. Exit
+Your Option: --------- ''')
+
+            if userOption == "6":
+                break
+            elif userOption == "1":
+                self.add_note()
+            elif userOption == "2":
+                self.show_all_notes()
+            elif userOption == "3":
+                self.show_note()
+            elif userOption == "4":
+                self.update_note()
+            elif userOption == "5":
+                self.delete_note()
+            else:
+                print("Invalid Option!")
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app = ConsoleApp()
 
     app.run()
-
-    app.show_notes()
-
-    app.show_note()
