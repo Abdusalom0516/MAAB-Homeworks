5ad1dc40dc2cd17a92df6f1aa4c9b323181d98f1
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2026-01-26 16:17:51 +0500

    Finish Lesson 11 HW Task 3.

--- lesson_11/homework.py
+++ lesson_11/homework.py
@@ -60,3 +60,79 @@ print(f"{sum(temperatures) / len(temperatures)}°C")
 
 
 
+
+# Task 3
+import json
+print("------------------------------")
+#  1. **Navigate to the Website:**
+#    - Visit the [Demoblaze homepage](https://www.demoblaze.com/).
+#    - Click on the **Laptops** section to view the list of available laptops.
+
+# 2. **Navigate to the Next Page:**
+#    - After reaching the Laptops section, locate and click the **Next** button to navigate to the next page of laptop listings.
+
+# 3. **Data to Scrape:**
+#    For each laptop on the page, scrape the following details:
+#    - **Laptop Name**
+#    - **Price**
+#    - **Description**
+
+# 4. **Data Storage:**
+#    - Save the extracted information in a structured **JSON format** with fields like:
+#      json
+#      [
+#        {
+#          "name": "Laptop Name",
+#          "price": "Laptop Price",
+#          "description": "Laptop Description"
+#        },
+#        ...
+#      ]
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time # I am gonna use it to add waiting time to make it clear to myself whta is happening in my code.
+import os
+
+chrome_options = Options()
+
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(chrome_options)
+
+# 1.
+driver.get("https://www.demoblaze.com/")
+
+
+laptop_categories = driver.find_elements(by=By.ID, value="itemc")
+
+laptop_categories[1].click()
+time.sleep(3)
+
+next_btn = driver.find_element(by=By.ID, value="next2")
+next_btn.click()
+time.sleep(3)
+
+laptops = driver.find_elements(by=By.CLASS_NAME, value="card-block")
+
+json_laptops = []
+
+
+for laptop in laptops:
+    split_list = laptop.text.split(f"\n")
+
+    json_laptops.append({ "name": split_list[0],
+         "price": split_list[1],
+         "description":split_list[2]})
+
+
+with open("laptops.json", "a") as f:
+        json.dump(json_laptops, f, indent=4)
+
+
+
+
+
+
