b49e4da13bc8eb03f5dd34c37f166a263433251b
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2026-02-03 15:03:30 +0500

    Create Lesson 15 Folder.

--- tasks.py
+++ tasks.py
@@ -1,1 +1,22 @@
 
+# **Instructions:**
+
+# 1. **Flip the Image**:
+#    - Flip the image horizontally and vertically (left-to-right and up-to-down).
+
+# 2. **Add Random Noise**:
+#    - Add random noise to the image.
+
+
+# **Requirements:**
+# - Use the **PIL** module onyl to:
+#   - Read the image.
+#   - Convert numpy array to image.
+#   - Save the modified image back to a file.
+# - Perform all manipulations using NumPy functions. Avoid using image editing functions from PIL or other libraries.
+
+
+# **Bonus Challenge**:
+# - Create a function for each manipulation (e.g., `flip_image`, `add_noise`, `brighten_channels`, `apply_mask`) to promote modularity and reusability of code.
+
+# ---
