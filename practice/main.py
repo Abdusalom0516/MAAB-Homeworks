865adc7d4bcc58d5de1dc34ef63c761a1d63d96f
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2026-02-10 13:02:55 +0500

    Start Practicing For Exam.

---
+++ practice/main.py
@@ -0,0 +1,53 @@
+# Task 1.
+# data = ["10", "25", "x", "42", "", "17", "error", "99"]
+
+# valid_numbers = []
+# sum_valid_numbers = 0
+# avg = 0
+
+
+# for elem in data:
+#     try:
+#         valid_numbers.append(int(elem))
+#         sum_valid_numbers += valid_numbers[-1]
+#     except:
+#         pass
+
+# avg = sum_valid_numbers / len(valid_numbers)
+
+
+# print(valid_numbers)
+# print(sum_valid_numbers)
+# print(avg)
+
+
+# Task 2.
+# def get_max(numbers_list: list[int]) -> int:
+#     max_number =  numbers_list[0]
+#     for elem in numbers_list:
+#         if elem > max_number:
+#             max_number = elem
+#     return max_number
+
+# def get_min(numbers_list: list[int]) -> int:
+#     min_number = numbers_list[0]
+#     for elem in numbers_list:
+#         if min_number > elem:
+#             min_number = elem
+#     return min_number
+
+
+# def get_avg(numbers_list: list[int]) -> float:
+#     numbers_sum = sum(numbers_list)
+#     return numbers_sum / len(numbers_list)
+
+
+# numbers_list = [15, 12, 8, 10]
+
+# print(get_max(numbers_list=numbers_list))
+# print(get_min(numbers_list=numbers_list))
+# print(get_avg(numbers_list=numbers_list))
+
+
+
+
